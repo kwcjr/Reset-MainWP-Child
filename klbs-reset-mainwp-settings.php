cad5aeabac36e5e9ca4840e18b44f4e037bdd864
Keith Crain <keith@kronoslabs.io>
2019-10-17 14:18:19 -0400

    code refactoring & addition of admin alert & plugin auto deactivation when done.

--- klbs-reset-mainwp-settings.php
+++ klbs-reset-mainwp-settings.php
@@ -1,50 +1,50 @@
 <?php
 /**
- * Plugin Name: Reset mainWP Settings
+ * Plugin Name: Reset mainWP-Child
  * Plugin URI: https://bitbucket.org/kronoslabs/klbs-reset-mainwpchild-settings/src
  * Description: Reset mainWP-Child Plugin Settings.
- * Author: Keith Crain
- * Author URI: Kronoslabs.io
+ * Author: Kronoslabs.io
+ * Author URI: https://www.Kronoslabs.io
  * Version: 1.0.0
  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-// load wp
-define('WP_USE_THEMES', false);
-require('../../../wp-load.php');
+function klbs_reset_mainWP_child() {
 
-// grab wpdb Global Variable
-global $wpdb;
+    // grab wpdb Global Variable
+    global $wpdb;
 
-// grab wpdb table prefix being used in this installation
-$table_name = "{$wpdb->prefix}options";
+    // grab wpdb table prefix being used in this installation
+    $table_name = "{$wpdb->prefix}options";
 
-// prepared statment == SQL DELETE FROM `prefix_options` WHERE option_name LIKE '%mainwp_%'
-$wpdb->query( 
-	$wpdb->prepare( 
-		"
-            DELETE FROM $table_name
-            WHERE option_name
-            LIKE %s
-		",
-	    '%mainwp_%'
-    )
-);
+    // prepared statment == SQL DELETE FROM `prefix_options` WHERE option_name LIKE '%mainwp_%'
+    $wpdb->query( 
+        $wpdb->prepare( 
+            "
+                DELETE FROM $table_name
+                WHERE option_name
+                LIKE %s
+            ",
+            '%mainwp_%'
+        )
+    );
 
-// This plugin will self-destruct Miss Moneypenny!
-// Recursivly delete the plugin folder
-function rrmdir($dir) {
-    if (is_dir($dir)) {
-      $objects = scandir($dir);
-      foreach ($objects as $object) {
-        if ($object != "." && $object != "..") {
-          if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
-        }
-      }
-      reset($objects);
-      rmdir($dir);
+}
+
+// Run the function
+klbs_reset_mainWP_child();
+
+// Display Admin notice
+function general_admin_notice(){
+    global $pagenow;
+    if ( $pagenow == 'plugins.php' ) { ?>
+        <div class="notice notice-success is-dismissible">
+            <p><?php _e('Default mainWP settings have been restored &amp; this plugin has been deactivated. Please click the delete button to remove it.', 'mwprs'); ?></p>
+        </div>
+        <?php
+
+        deactivate_plugins( plugin_basename( __FILE__ ) );
     }
- } 
-
- rrmdir( plugin_dir_path( __FILE__ ) );
+}
+add_action('admin_notices', 'general_admin_notice');
