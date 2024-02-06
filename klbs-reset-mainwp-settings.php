3044b6b29a033b9c17a9fadacd506d21910c6ea5
Keith Crain <keith@kronoslabs.io>
2024-02-05 23:55:18 -0500

    Updated for use with MainWP v5

--- klbs-reset-mainwp-settings.php
+++ klbs-reset-mainwp-settings.php
@@ -1,14 +1,14 @@
 <?php
-/**
- * Plugin Name: Reset mainWP-Child
- * Plugin URI: https://bitbucket.org/kronoslabs/klbs-reset-mainwpchild-settings/src
- * Description: Reset mainWP-Child Plugin Settings.
- * Author: Kronoslabs.io
- * Author URI: https://www.Kronoslabs.io
- * Version: 1.0.1
- * Tested with: MainWP v4.3
- * Tested with WordPress v6.1.1
- */
+	/**
+	 * Plugin Name: Reset MainWP-Child
+	 * Plugin URI: https://bitbucket.org/kronoslabs/klbs-reset-mainwpchild-settings/src
+	 * Description: Reset MainWP-Child Plugin Settings.
+	 * Author: Keith Crain
+	 * Author URI: https://github.com/kwcjr
+     * Tested with: MainWP-Child : v5.0.0
+	 * Tested with WordPress     : v6.4.3
+     * Version: 1.0.2
+	 */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
@@ -20,7 +20,7 @@ function klbs_reset_mainWP_child() {
     // grab wpdb table prefix being used in this installation
     $table_name = "{$wpdb->prefix}options";
 
-    // prepared statment == SQL DELETE FROM `prefix_options` WHERE option_name LIKE '%mainwp_%'
+    // prepared statement == SQL DELETE FROM `prefix_options` WHERE option_name LIKE '%mainwp_%'
     $wpdb->query( 
         $wpdb->prepare( 
             "
@@ -32,6 +32,7 @@ function klbs_reset_mainWP_child() {
         )
     );
 
+	// prepared statement == SQL DELETE FROM `prefix_postmeta` WHERE option_name LIKE '%mainwp_%'
     $table_name = "{$wpdb->prefix}postmeta";
     $wpdb->query( 
         $wpdb->prepare( 
@@ -52,12 +53,15 @@ klbs_reset_mainWP_child();
 // Display Admin notice
 function general_admin_notice(){
     global $pagenow;
-    if ( $pagenow == 'plugins.php' ) { ?>
-        <div class="notice notice-success is-dismissible">
-            <p><?php _e('Default mainWP settings have been restored &amp; this plugin has been deactivated. Please click the delete button to remove it.', 'mwprs'); ?></p>
+    if ( $pagenow == 'plugins.php' ) {
+    ?>
+        <div class="notice notice-success">
+            <p><?php esc_html_e( 'Default MainWP settings have been restored &amp; this plugin has been deactivated. Please click the delete button to remove it!', 'mwprs' ); ?></p>
         </div>
-        <?php
-
+        <div class="notice notice-warning">
+            <p><?php esc_html_e( 'Please remove the Reset MainWP-Child Plugin from this website after use!', 'mwprs' ); ?></p>
+        </div>
+    <?php
         deactivate_plugins( plugin_basename( __FILE__ ) );
     }
 }
